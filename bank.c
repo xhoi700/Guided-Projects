6e5acb0d50832d409c2d82104bac2e1dce0e4ee2
xhoi700 <10057@holbertonstudents.com>
2024-12-16 19:13:11 +0000

    new

---
+++ bank.c
@@ -0,0 +1,12 @@
+#include "bank.h"
+#include <stdio.h>
+
+void viewBalance(BankClient clients[], int size, int accountNumber)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Account balance is %s\n", )
+    }
+}
