fcb8be5cffcb4e68688b337800aa4851320187bd
xhoi700 <10057@holbertonstudents.com>
2024-12-16 19:13:54 +0000

    new

--- bank.c
+++ bank.c
@@ -4,9 +4,25 @@
 void viewBalance(BankClient clients[], int size, int accountNumber)
 {
     int i;
+    int found = 0;
 
     for (i = 0; i < size; i++)
     {
-        printf("Account balance is %s\n", )
+        if (clients[i] == accountNumber)
+        {
+            printf("Account balance is %.2f", clients[i].balance);
+        }
+        found = 1;
+        break;
+    }
+}
+
+void deposit(BankClient clients[], int size, int accountNumber, float amount)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        
     }
 }
