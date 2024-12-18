17090fdf4e58c7651119b3cadd485e39cb4b1c24
xhoi700 <10057@holbertonstudents.com>
2024-12-18 09:39:59 +0000

    first taks

---
+++ loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 16;
+
+    if (i % 2 == 0)
+    {
+        printf("the number is even %d", i);
+    }
+    else
+    {
+        printf("the number is odd %d", i);
+    }
+     return (0);
+}
+   
